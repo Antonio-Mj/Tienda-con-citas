1146fce6fbd5115d545779678225a8169d4f647a
Antonio-Mj <antonio.devpisc@gmail.com>
2024-06-22 13:29:32 -0600

    first commit

--- Controller/bd_conexion.php
+++ Controller/bd_conexion.php
@@ -2,7 +2,7 @@
 $usuario = 'root';
 $contraseña = '';
 try {
-    $conn = new PDO('mysql:host=localhost;dbname=store', $usuario, $contraseña);
+    $conn = new PDO('mysql:host=localhost;dbname=tienda-uñas', $usuario, $contraseña);
 
 } catch (PDOException $e) {
     print "¡Error!: " . $e->getMessage() . "<br/>";
